848e26aeea81cb9704473d027ed569dca35db097
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-16 20:56:37 +0100

    Function - return value

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+//Only change code below this line
+function xSix(num) {
+
+return num * 6;
+}
+var answer = xSix(5);
+var answer = xSix(2);
+var answer = xSix(0);
+
+console.log(xSix(5));  //Change this line
+console.log(xSix(2));  //Change this line
+console.log(xSix(0));  //Change this line
+
+//Only change code above this line
+
+module.exports = xSix;
