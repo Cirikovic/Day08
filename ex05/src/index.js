5ab052efd05b9195d617e1d51c0ac953448d7bb3
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-16 20:43:07 +0100

    little dress2

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,7 +1,7 @@
 // Only change code below this line
 var dress = "little dress"
 function myDress() {
-var dress = "ball gown"
+dress = "ball gown"
 
     //Only change code above this line
     return dress;
