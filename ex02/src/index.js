9250835f57614a244c3844be9cf480e2565617d0
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-16 10:13:07 +0100

    function with two arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function functionPrintInfo(name, age) {
+console.log(name, age);
+}
+var name = "John";
+var age = 23;
+var name = "Michael";
+var age = 43;   
+
+
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+module.exports = functionPrintInfo;
